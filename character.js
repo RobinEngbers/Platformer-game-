c68ede54e14fd1eed121ffa6b5e7177505264afe
Robin Engbers <90309164+RobinEngbers@users.noreply.github.com>
2022-02-02 11:40:37 +0000

    blokje in scherm

---
+++ character.js
@@ -0,0 +1,14 @@
+class Character {
+  constructor(show) {
+
+  }
+  show() {
+    rect(20, 330, 20, 20);
+    fill("green");
+  }
+
+  move() {
+    
+  }
+  
+}
--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 var character;
+var collision
 
 function setup() {
   createCanvas(400, 400);
+  character = new Character()
 }
 
 function draw() {
   background("#3BC7FA");
   rect(0, 350, 500, 55);
-  fill("green");
+  fill("red");
 
-  rect(20, 330, 20, 20);
+  character.show();
 }
