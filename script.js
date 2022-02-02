6f2c43b3b9b7dee0fdf9c61237f052d144ff2851
RobinEngbers <90309164+RobinEngbers@users.noreply.github.com>
2022-02-02 11:07:45 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var character;
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background("#3BC7FA");
+  rect(0, 350, 500, 55);
+  fill("green");
+
+  rect(20, 330, 20, 20);
+}
